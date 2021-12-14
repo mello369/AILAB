658c6d9ca22c4962072a8682549dbf39129ab9d2
Naman Singh <55326403+mello369@users.noreply.github.com>
2021-12-14 18:00:41 +0530

    Update vacuum.py

--- vacuum_cleaner/vacuum.py
+++ vacuum_cleaner/vacuum.py
@@ -5,26 +5,65 @@ def display(room):
 
 room = [
     [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
-    [1, 1, 1, 1],
+    [1, 0, 1, 1],
+    [1, 1, 0, 1],
+    [1, 0, 1, 1],
 ]
+rows=len(room)
+col=len(room[0])
+tiles=rows*col
+cost=0
 print("1 : Dirty Tile  0 : Clean Tile")
 display(room)
 
-x =0
-y= 0
-z=0
-while x < 4:
-    while y < 4:
-        ch = random.choice([0,1])
-        if ch == 1 :
-            print("Vaccuming in location : ",x, y)
-            room[x][y] = 0
-            z+=1
-        y+=1
-    x+=1
-    y=0
-pro= (100-((z/16)*100))
-display(room)
-print('performance=',pro,'%')
+print("Enter initial position of the vacuum cleaner : ")
+x=int(input())
+y=int(input())
+
+
+while y>1:
+    if room[x-1][y-1] == 1 :
+        print("Cleaning location ",(x,y))
+        room[x-1][y-1]=0
+    else :
+        print("Location ",(x,y)," is already clean")
+    y-=1
+    cost+=1
+
+while x>1 :
+    if room[x-1][y-1] == 1 :
+        print("Cleaning location ",(x,y))
+        room[x-1][y-1]=0
+    else :
+        print("Location ",(x,y)," is already clean")
+    x-=1
+    cost+=1
+
+
+vis_tiles=0
+d=0
+while vis_tiles != tiles :
+    if room[x-1][y-1] == 1 :
+        print("Cleaning location ",(x,y))
+        room[x-1][y-1]=0
+    else :
+        print("Location ",(x,y)," is already clean")
+    vis_tiles+=1
+    if d == 0 :
+        if y == col :
+            if x<rows :
+                x+=1
+                d=1
+        else :
+            y+=1
+    
+    else :
+        if y == 1 :
+            if x<rows :
+                x+=1
+                d=0
+            
+        else :
+            y-=1
+    cost+=1
+print("Total Cost(Distance travelled : )",cost)
